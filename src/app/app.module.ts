4736af02022446044890b4af07f61e1344253d1d
lacrotas <123123maks1@mail.ru>
2023-09-26 20:48:28 +0300

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from "@angular/common/http";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: []
+  //   CUSTOM_ELEMENTS_SCHEMA
+  // ]
+})
+export class AppModule { }
+// CUSTOM_ELEMENTS_SCHEMA
+//"src/assets/lottie-player.js"
